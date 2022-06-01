48f97a4a0091d32cd41c29e0076053d320513a0d
Prasad More <prasad.more@apmosys.com>
2022-06-01 16:08:49 +0530

    refactor : JPA native query

--- src/main/java/com/apmosys/employeeportal/repository/JobRoleRepository.java
+++ src/main/java/com/apmosys/employeeportal/repository/JobRoleRepository.java
@@ -11,11 +11,7 @@ import com.apmosys.employeeportal.model.JobRole;
 @Repository
 public interface JobRoleRepository extends JpaRepository<JobRole, Long> {
 	
-	@Query(value = "SELECT j.name as jobrolename ,\r\n"
-			+ "e.name as createdby,j.created_on,d.name  as department\r\n"
-			+ "FROM job_role j \r\n"
-			+ "INNER JOIN employee e ON j.created_by = e.emp_id\r\n"
-			+ "INNER JOIN department d ON d.dept_id = j.dept_id",nativeQuery = true)
+	@Query(nativeQuery = true)
 	public List<Object[]> getAllJobRoles();
 
 }
