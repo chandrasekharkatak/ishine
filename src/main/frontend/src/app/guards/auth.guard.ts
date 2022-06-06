ec0b80a8ba1fb8d43acf5407ff8b65e75c3aaac5
Prasad More <prasad.more@apmosys.com>
2022-06-06 15:35:24 +0530

    refactor : updated auth guard according to feature role mapping.

--- src/main/frontend/src/app/guards/auth.guard.ts
+++ src/main/frontend/src/app/guards/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { User } from '../models/user';
 import { AuthenticationService } from '../services/authentication.service';
 
 @Injectable({
@@ -17,9 +18,14 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      const currentUser = this.authenticationService.currentUserValue;
+      const currentUser:User = this.authenticationService.currentUserValue;
       if (currentUser) {
-         return true;    
+        if(!currentUser.tabList.find(tab => tab.tabRouteName == route.routeConfig.path)){
+          // role not authorised so redirect to home page
+          this.router.navigate(['/user-profile']); // home not mapped for default features yet
+          return false;
+        }   
+        return true;
       }else{
           this.router.navigate(['/login'], { queryParams: { }});
           return false;
