daaff070f8f41c5f29540bb6a30b0429016caaac
Prasad More <prasad.more@apmosys.com>
2022-06-24 15:46:03 +0530

    feat: All Request div

--- src/main/frontend/src/app/home/home.component.ts
+++ src/main/frontend/src/app/home/home.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { first } from 'rxjs/operators';
+import { Leave } from '../models/leave';
+import { User } from '../models/user';
+import { AuthenticationService } from '../services/authentication.service';
+import { LeaveService } from '../services/leave.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +13,109 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  //modal 
+  alertMessage:any;
+  modalRef: BsModalRef = new BsModalRef();
+
+  feature="Home";
+  currentUser:User;
+  userMapping:any = {};
+
+  isReqPending:boolean = true;
+  leaveApplicationCount:any = 0;
+  leaveApplicationList:any[] = [];
+
+  compOffApplicationCount:any = 0;
+  allCompOffApplications:any[] = [];
+
+  constructor(
+    private modalService: BsModalService,
+    private authenticationService : AuthenticationService,
+    private leaveService : LeaveService,
+  ) {
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+   }
 
   ngOnInit(): void {
+    this.getAllMyTeamsPendingLeaveApplicationsByManagerId();
+    this.getPendingCompOffRequestsByManagerId();
   }
 
+  // Leave Applications
+  getAllMyTeamsPendingLeaveApplicationsByManagerId(){
+    this.leaveApplicationList = []
+
+    let leaveObj = new Leave();
+    leaveObj.managerId = this.currentUser.empId;
+    this.leaveService.getAllMyTeamsPendingLeaveApplicationsByManagerId(leaveObj).pipe(first()).subscribe((response: any) => {
+      if (response.serviceStatus == "Success") {
+        this.leaveApplicationList = response.serviceResponse;
+        console.log("leaveApplicationList : ", this.leaveApplicationList);
+        this.leaveApplicationCount =  this.leaveApplicationList.length;
+      } else {
+        console.error(response.serviceResponse);
+      }
+    });
+  }
+
+  onUpdateLeaveStatus(template: TemplateRef<any>, leaveApplication, updatedLeaveStatusId){
+    // 1 = pending , 2 = Approved , 3= Rejected
+    leaveApplication.leaveStatusId = updatedLeaveStatusId;
+    console.log("leaveApplication : ", leaveApplication);
+    
+    this.leaveService.updateLeaveStatus(leaveApplication).pipe(first()).subscribe((response: any) => {
+      if (response.serviceStatus == "Success") {
+        this.openAlertMod(template, response.serviceResponse);
+        this.getAllMyTeamsPendingLeaveApplicationsByManagerId()
+      } else {
+        this.openAlertMod(template, response.serviceResponse);
+      }
+    });
+  }
+
+
+  //comOff Applications
+  getPendingCompOffRequestsByManagerId(){
+    this.allCompOffApplications = []
+
+    let compOff = new Leave();
+    compOff.managerId = this.currentUser.empId;
+    this.leaveService.getPendingCompOffRequestsByManagerId(compOff).pipe(first()).subscribe((response: any) => {
+      if (response.serviceStatus == "Success") {
+        this.allCompOffApplications = response.serviceResponse;
+        this.compOffApplicationCount =  this.allCompOffApplications.length;
+        console.log("allCompOffApplications : ", this.allCompOffApplications);
+      } else {
+        console.error(response.serviceResponse);
+      }
+    });
+  }
+
+  onUpdateCompOffStatus(template: TemplateRef<any>, compOffObj, updatedCompOffStatusId){
+    // 1 = pending , 2 = Approved , 3= Rejected
+    compOffObj.leaveStatusId = updatedCompOffStatusId;
+    console.log("Update Comp off : ", compOffObj);
+    this.leaveService.updateCompOffById(compOffObj).pipe(first()).subscribe((response: any) => {
+      if (response.serviceStatus == "Success") {
+        this.openAlertMod(template, response.serviceResponse);
+        this.getPendingCompOffRequestsByManagerId();
+      } else {
+        this.openAlertMod(template, response.serviceResponse);
+      }
+    });
+  }
+
+  // modals
+  openReqMod(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, { class: 'modal-xl' });
+  }
+
+  openAlertMod(template: TemplateRef<any>, message: any) {
+    this.modalRef = this.modalService.show(template, { class: 'modal-sm' });
+    this.alertMessage = message;
+  }
+
+  cancelRequest() {
+    this.modalRef.hide();
+  }
 }
