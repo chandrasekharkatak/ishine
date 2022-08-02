463a4e26e8d173f54fe016dc8f5b8f9baee027da
Prasad More <prasad.more@apmosys.com>
2022-08-02 12:23:03 +0530

    Updated Timesheet Lock days

--- src/main/frontend/src/app/user-timesheet/my-timesheet/my-timesheet.component.ts
+++ src/main/frontend/src/app/user-timesheet/my-timesheet/my-timesheet.component.ts
@@ -162,8 +162,9 @@ export class MyTimesheetComponent implements OnInit {
     const time=checkDate?.getTime();
     let currentDate = new Date();
 
+    // 7 days + 1 current Day
     let endDate = currentDate;
-    let startDate = new Date(endDate.getTime() - (this.currentUser.timesheetLockDays * DAY_IN_MS));
+    let startDate = new Date(endDate.getTime() - ((this.currentUser.timesheetLockDays+1) * DAY_IN_MS));
     
     return (checkDate <= endDate && checkDate >= startDate && !this.availableTimesheets.find(timesheet => timesheet.date == this.datePipe.transform(checkDate, "YYYY-MM-dd"))) ? true : false;
   }
