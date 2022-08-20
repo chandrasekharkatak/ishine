0ce0ba7e9bda83bb54379fce6b47c37bcd880109
Suraj Honavar <suraj.honavar@apmosys.com>
2022-08-20 13:18:17 +0530

    Bug fixes

--- src/main/java/com/apmosys/employeeportal/model/EmployeeLeavesMap.java
+++ src/main/java/com/apmosys/employeeportal/model/EmployeeLeavesMap.java
@@ -19,7 +19,7 @@ public class EmployeeLeavesMap {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Short employeeLeavesMapId;
+	private Long employeeLeavesMapId;
 	
 	private Long empId;
 	
