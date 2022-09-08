14e70e5965b0d21ef5bd6b42ed20cfdb618a26fb
Harshit Toxia <harshit.toxia@apmosys.com>
2022-09-08 17:10:49 +0530

    added sendMailWithCC in mailService

--- src/main/java/com/apmosys/employeeportal/service/MailService.java
+++ src/main/java/com/apmosys/employeeportal/service/MailService.java
@@ -61,7 +61,56 @@ public class MailService {
 			msg.setContent(text, "text/html");
 			msg.setFrom(new InternetAddress(sender));
 
-			msg.setRecipients(javax.mail.Message.RecipientType.TO, InternetAddress.parse(receiver));
+			msg.setRecipients(javax.mail.Message.RecipientType.TO, InternetAddress.parse(receiver, true));
+
+			javax.mail.Transport.send(msg);
+
+			return true;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+
+		}
+	}
+	
+	public boolean sendMailWithCC(String receiver,String cc, String subject, String text) throws AddressException, MessagingException {
+
+		try {
+
+			Properties props = new Properties();
+
+			props.put("mail.smtp.user", sender);
+			props.put("mail.smtp.host", host);
+			props.put("mail.smtp.port", port);
+			props.put("mail.smtp.starttls.enable", "false");
+			props.put("mail.smtp.auth", "true");
+			props.put("mail.smtp.socketFactory.port", port);
+			props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+			props.put("mail.smtp.socketFactory.fallback", "false");
+			props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+
+			props.remove("mail.smtp.socketFactory.class");
+			props.setProperty("mail.smtp.starttls.enable", "true");
+			SecurityManager security = System.getSecurityManager();
+			System.out.println("Before Authentication");
+			Authenticator auth = new Authenticator() {
+
+				public PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(sender, pwd);
+				}
+			};
+			System.out.println("After Authentication " + auth);
+			javax.mail.Session session = javax.mail.Session.getInstance(props, auth);
+
+			MimeMessage msg = new MimeMessage(session);
+
+			msg.setSubject(subject);
+			msg.setContent(text, "text/html");
+			msg.setFrom(new InternetAddress(sender));
+
+			msg.setRecipients(javax.mail.Message.RecipientType.TO, InternetAddress.parse(receiver, true));
+			msg.setRecipients(javax.mail.Message.RecipientType.CC, InternetAddress.parse(cc, true));
 
 			javax.mail.Transport.send(msg);
 
