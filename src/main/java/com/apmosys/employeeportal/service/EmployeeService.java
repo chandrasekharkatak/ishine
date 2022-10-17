fff0f97822f35fbc04c87da7266ea3a68c1cf9c7
Suraj Honavar <suraj.honavar@apmosys.com>
2022-10-17 17:25:44 +0530

    test

--- src/main/java/com/apmosys/employeeportal/service/EmployeeService.java
+++ src/main/java/com/apmosys/employeeportal/service/EmployeeService.java
@@ -1935,8 +1935,7 @@ public class EmployeeService {
 				} else {
 					List<EmployeeDTO> empDTO = new ArrayList<>();
 
-					list.forEach((object) -> {
-						
+					list.forEach((object) -> {						
 						EmployeeDTO dto = new EmployeeDTO();
 						dto.setManagerId(object[0] != null ? Long.parseLong(object[0].toString()) : null);
 						empDTO.add(dto);
