acd1954c80fbae02639379e6134d8df6e1dc973e
Harshit Toxia <harshit.toxia@apmosys.com>
2023-02-14 13:14:01 +0530

    bug fixes : 812(#1408) - activity

--- src/main/frontend/src/app/user-team/team-config/team-config.component.ts
+++ src/main/frontend/src/app/user-team/team-config/team-config.component.ts
@@ -685,7 +685,7 @@ export class TeamConfigComponent implements OnInit {
 
   getDepartmentByTeam(teamId: any){
     this.filteredDeptList = [];
-    this.activityObj.departmentList = '';
+    this.activityObj.departmentList = ''; 
 
     const selectedTeam = this.allTeamList.find(x => x.teamId == teamId);
     let departmentList = selectedTeam.departmentList.map(x => +x);
