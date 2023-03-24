4719b46497965c701123743bd921d72fbafe59c6
Prasad More <prasad.more@apmosys.com>
2023-03-24 12:19:18 +0530

    bug : resetting approval To if reporting manager is removed

--- src/main/frontend/src/app/configuration/employee-config/employee-config.component.ts
+++ src/main/frontend/src/app/configuration/employee-config/employee-config.component.ts
@@ -1323,6 +1323,10 @@ export class EmployeeConfigComponent implements OnInit {
     }
 
     employee.specializationList = this.employeeObj.specializationList;
+    if(this.employeeObj.reportingManagerId == null || this.employeeObj.reportingManagerId == ""){
+      employee.reportingManagerId = null;
+      employee.approvalsTo = null;
+    }
 
     this.employeeService.updateEmployee(employee).pipe(first()).subscribe((response: any) => {
       if (response.serviceStatus == "Success") {
