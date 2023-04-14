b9bc30bc10ef91abc178df7c2194086efee05e36
Harshit Toxia <harshit.toxia@apmosys.com>
2023-04-14 16:26:06 +0530

    Home config UI consolidation

--- src/main/frontend/src/app/configuration/home-config/home-config.component.ts
+++ src/main/frontend/src/app/configuration/home-config/home-config.component.ts
@@ -120,12 +120,18 @@ export class HomeConfigComponent implements OnInit {
   }
 
   sectionViewInit() {
-    if(this.userMapping.upload_event_photos){
-      this.showUploadPhotosForm();
-    }else if(this.userMapping.view_all_event_photos || this.userMapping.delete_event_photos){
+    // if(this.userMapping.upload_event_photos){
+    //   this.showUploadPhotosForm();
+    // }else if(this.userMapping.view_all_event_photos || this.userMapping.delete_event_photos){
+    //   this.showTable();
+    // }else if(this.userMapping.set_notifications){
+    //   this.showNotificationForm();
+    // }
+
+    if(this.userMapping.view_all_event_photos || this.userMapping.delete_event_photos){
       this.showTable();
-    }else if(this.userMapping.set_notifications){
-      this.showNotificationForm();
+    }else if(this.userMapping.view_all_notification){
+      this.showNotificationTable();
     }
 
   }
