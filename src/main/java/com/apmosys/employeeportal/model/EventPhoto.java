722f34e5d7494b56ab7f363027964980af70a785
Prasad More <prasad.more@apmosys.com>
2023-07-03 15:44:15 +0530

    change : updated length of caption column to 500 characters

--- src/main/java/com/apmosys/employeeportal/model/EventPhoto.java
+++ src/main/java/com/apmosys/employeeportal/model/EventPhoto.java
@@ -1,5 +1,6 @@
 package com.apmosys.employeeportal.model;
 
+import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -27,6 +28,7 @@ public class EventPhoto  {
 	
 	private String imageName;
 	
+	@Column(length = 500)
 	private String eventCaption;
 	
 	private Integer photoOrder;
