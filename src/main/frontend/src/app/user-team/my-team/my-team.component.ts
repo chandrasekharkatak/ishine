ae88b53fa09b52800d85c9ac30255a20ea7b5eca
Priyadarshini Singh <priyadarshini.singh@apmosys.com>
2024-08-28 17:44:07 +0530

    Empty Team List Handling

--- src/main/frontend/src/app/user-team/my-team/my-team.component.ts
+++ src/main/frontend/src/app/user-team/my-team/my-team.component.ts
@@ -911,6 +911,7 @@ export class MyTeamComponent implements OnInit {
       leaveObj.fromDate = this.fromDate;
       leaveObj.toDate = this.toDate;
       leaveObj.deptId = this.currentUser.departmentId;
+      leaveObj.employeeRole=this.currentUser.employeeRole;
       this.teamViewService.getDepartmentLeaveHistory(leaveObj).pipe(first()).subscribe((response: any) => {
         if (response.serviceStatus == "Success") {
           this.departmentLeaveHistoryList = response.serviceResponse;
