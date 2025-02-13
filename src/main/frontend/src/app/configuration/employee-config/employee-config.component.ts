d90cd5721538dd9efaad673c02d72b2ec13ec645
rahul.singh@apmosys.com <rahul.singh@apmosys.com>
2025-02-13 15:23:57 +0530

    above the 18 year

--- src/main/frontend/src/app/configuration/employee-config/employee-config.component.ts
+++ src/main/frontend/src/app/configuration/employee-config/employee-config.component.ts
@@ -278,6 +278,39 @@ export class EmployeeConfigComponent implements OnInit {
       history.pushState(null, null, location.href);
     })
   }
+  validateBirthDate(template: TemplateRef<any>) {
+    let birthdate = new Date(this.employeeObj.dateOfBirth);
+    let dtCurrent = new Date();
+    let flag = true;
+    let dobInput: any = document.getElementById('DOB');
+
+   if (dtCurrent.getFullYear() - birthdate.getFullYear() < 18) {
+      this.openAlertMod(template, 'Employee age cannot be less than 18 years.');
+      flag = false;
+    }
+    else if (dtCurrent.getFullYear() - birthdate.getFullYear() == 18) {
+
+      //CD: 11/06/2018 and DB: 15/07/2000. Will turned 18 on 15/07/2018.
+      if (dtCurrent.getMonth() < birthdate.getMonth()) {
+        this.openAlertMod(template, 'Employee age cannot be less than 18 years.');
+        flag = false;
+      }
+
+      if (dtCurrent.getMonth() == birthdate.getMonth()) {
+        //CD: 11/06/2018 and DB: 15/06/2000. Will turned 18 on 15/06/2018.
+        if (dtCurrent.getDate() < birthdate.getDate()) {
+          this.openAlertMod(template, 'Employee age cannot be less than 18 years.');
+          flag = false;
+        }
+      }
+    }
+    if (!flag) {
+      setTimeout(() => {
+        dobInput.value = '';
+        this.employeeObj.dateOfBirth = '';
+      }, 10)
+    }
+  }
 
   ngOnDestroy() {
     if (this.subscription) {
