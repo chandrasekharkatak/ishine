be4a8e7c6cf05ec138254e860b3d9b811bee4c3d
Priyadarshini Singh <priyadarshini.singh@apmosys.com>
2025-03-01 12:28:50 +0530

    Portal Configular with Multiple Department and Employee for Leave deduction

---
+++ src/main/frontend/src/app/models/biomaxRequest copy.ts
@@ -0,0 +1,16 @@
+export class biomaxRequest{
+    biomaxreequestId:any;
+    biomaxTitle:any;
+    biomaxTitleValue:any;
+    empId:any;
+    reportingManagerId:any;
+    biomaxrequestDate:any;
+    biomaxStatus:any;
+    createdOn:any;
+    statusBy:any;
+    requestRemark:any;
+    statusDate:any;
+    reportingManagerName:any;
+    empName:any;
+
+}
