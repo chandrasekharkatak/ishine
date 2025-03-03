b83f38f7abad0190f4f84a58ea6c7ea9310c3ca0
raj-alpha-swain <rajalphaswain786@gmail.com>
2025-03-03 16:15:41 +0530

    goal impl [changes in service]

--- src/main/java/com/apmosys/employeeportal/dto/ReviewTableDto.java
+++ src/main/java/com/apmosys/employeeportal/dto/ReviewTableDto.java
@@ -1,5 +1,25 @@
 package com.apmosys.employeeportal.dto;
 
-public class ReviewTableDto {
+import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.*;
+
+@Getter
+@Setter
+public class ReviewTableDto {
+	private Long employeeId;
+	private Long goalId;
+	private Long kpiId;
+	private Long reviewerId;
+	private Integer quarterId;
+	private Float rating;
+	private String remarks;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private LocalDate reviewDate;
+	
+	private Integer kpiScore;
+	
 }
--- src/main/java/com/apmosys/employeeportal/service/QuaterService.java
+++ src/main/java/com/apmosys/employeeportal/service/QuaterService.java
@@ -29,18 +29,18 @@ public class QuaterService {
 		}
 		 return quarterRepository.save(quarter);
 	}
-	public Boolean checkcompletiontime(LocalDate actualCompletionDate, LocalDate expectedCompletionDate)
-	{
-		if(actualCompletionDate == null || expectedCompletionDate == null) {return false;}
-		
-		boolean isOnTime = !actualCompletionDate.isAfter(expectedCompletionDate);
-		
-		if(isOnTime)
-		{
-			System.out.print("Done");
-		}
-		return isOnTime;
-	}
+//	public Boolean checkcompletiontime(LocalDate actualCompletionDate, LocalDate expectedCompletionDate)
+//	{
+//		if(actualCompletionDate == null || expectedCompletionDate == null) {return false;}
+//		
+//		boolean isOnTime = !actualCompletionDate.isAfter(expectedCompletionDate);
+//		
+//		if(isOnTime)
+//		{
+//			System.out.print("Done");
+//		}
+//		return isOnTime;
+//	}
 	
 	
 	
