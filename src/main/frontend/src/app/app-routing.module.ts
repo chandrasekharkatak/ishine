8cc958533cdea5ddf695906d27677f6ea43b2094
anyush panda <anyush.panda@apmosys.com>
2025-03-17 17:57:46 +0530

    merge resolve

--- src/main/frontend/src/app/app-routing.module.ts
+++ src/main/frontend/src/app/app-routing.module.ts
@@ -69,11 +69,8 @@ import { InformationPreviewComponent } from './user-update-info/information-prev
 import { UserUpdateInfoComponent } from './user-update-info/user-update-info.component';
 import { ProjectViewComponent } from './project-view/project-view.component';
 import { PerformanceConfigComponent } from './configuration/performance-config/performance-config.component';
-<<<<<<< HEAD
 import { TravelAllowanceComponent } from './travel-allowance/travel-allowance.component';
-=======
 import { ReimbursementComponent } from './reimbursement/reimbursement.component';
->>>>>>> d70348accb6e0391bd8323274a3554473d37bade
 
 const routes: Routes = [
   {path:'', redirectTo:'home', pathMatch:'full'},
