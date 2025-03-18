00e44939da32347d37149f6875980dc1b1f1c69b
anyush panda <anyush.panda@apmosys.com>
2025-03-18 17:23:34 +0530

    Reimbursement form complete

--- src/main/frontend/src/app/reimbursement/my-reimbursement/my-reimbursement.component.ts
+++ src/main/frontend/src/app/reimbursement/my-reimbursement/my-reimbursement.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/models/employee';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { EmployeeService } from 'src/app/services/employee.service';
 
 @Component({
   selector: 'app-my-reimbursement',
@@ -6,10 +9,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-reimbursement.component.css']
 })
 export class MyReimbursementComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+selectedReason:any;
+isTravel:boolean = false;
+isFood:boolean = false;
+ currentUser:any;
+  currentEmployeeInfo:Employee = new Employee();
+  constructor(private empService : EmployeeService, private authenticationService: AuthenticationService) { 
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
   }
 
+  ngOnInit(): void {
+    this.onGetEmployeeInfo();
+  }
+
+  onReasonSelect(){
+    console.log(this.selectedReason);
+    if(this.selectedReason === 'Travel'){
+      this.isTravel = true;
+      console.log(this.isTravel);
+    }
+    else if(this.selectedReason === 'Food'){
+      this.isFood = true;
+      this.isTravel = false;
+      console.log(this.isFood);
+    }
+    else{
+      this.isFood = false;
+      this.isTravel = false;
+    }
+  }
+
+  async onGetEmployeeInfo(){
+      this.currentEmployeeInfo = new Employee();
+      let currentEmp = new Employee();
+      currentEmp.empId = this.currentUser.empId;
+      currentEmp.isDraft = false;
+      //console.log("currentEmp : ", currentEmp);
+      
+      const response: any = await this.empService.getEmployeeByEmpId(currentEmp).toPromise();
+      if (response.serviceStatus == "Success") {
+        this.currentEmployeeInfo = response.serviceResponse;
+      
+        console.log("currentEmployeeInfo : ", this.currentEmployeeInfo);
+        //this.loadProfileImage(this.currentEmployeeInfo.imageBytes)
+  
+      } else {
+        console.error(response.serviceResponse);
+      }
+  
+      
+  
+      
+     
+    }
 }
