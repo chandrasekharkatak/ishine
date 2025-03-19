5cfb7d14ffe17c30ce8c581afeab22df228c60d1
anyush panda <anyush.panda@apmosys.com>
2025-03-19 17:00:03 +0530

    Update service

--- src/main/java/com/apmosys/employeeportal/service/TravelDeskService.java
+++ src/main/java/com/apmosys/employeeportal/service/TravelDeskService.java
@@ -54,11 +54,13 @@ public class TravelDeskService {
 		if(savedTravelDesk == null) {
 			serviceResponse.setServiceError("Unable to save..!!");
 			serviceResponse.setServiceStatus(ServiceResponse.STATUS_FAIL);
+			serviceResponse.setServiceMessage("Failed to save...!!");
 			return serviceResponse;
 		}
 		else {
 			serviceResponse.setServiceStatus(ServiceResponse.STATUS_SUCCESS);
 			serviceResponse.setServiceResponse(savedTravelDesk);
+			serviceResponse.setServiceMessage("Saved Successfully..!!");
 			return serviceResponse;
 		}
 		}
@@ -95,7 +97,51 @@ public class TravelDeskService {
 	public ServiceResponse updateTravelData(TravelDeskDTO travelData) {
 		ServiceResponse serviceResponse = new ServiceResponse();
 		try {
-			return serviceResponse;
+			TravelDesk existingTravelDesk = tavelDeskRepository.findByRequestId(travelData.getRequestId());
+			if(existingTravelDesk == null) {
+				serviceResponse.setServiceError("Data Not Found...!!");
+				serviceResponse.setServiceStatus(ServiceResponse.STATUS_FAIL);
+				return serviceResponse;
+			}
+			else {
+				existingTravelDesk.setEmpId(travelData.getEmployeeId());
+				existingTravelDesk.setEmail(travelData.getEmail());
+				existingTravelDesk.setMobileNo(travelData.getMobileNo());
+				existingTravelDesk.setDepartment(travelData.getDepartmentName());
+				existingTravelDesk.setName(travelData.getFullName());
+				existingTravelDesk.setRequestType(travelData.getAssociatedTravelRequest());
+				existingTravelDesk.setTravelMode(travelData.getTravelMode());
+				existingTravelDesk.setTravelClass(travelData.getTravelClass());
+				existingTravelDesk.setPurpose(travelData.getPurposeOfTravel());
+				existingTravelDesk.setFromLocation(travelData.getFromLocation());
+				existingTravelDesk.setFromDate(travelData.getFromDate());
+				existingTravelDesk.setToLocation(travelData.getToLocation());
+				existingTravelDesk.setToDate(travelData.getToDate());
+				existingTravelDesk.setAppliedBy(travelData.getEmployeeId());
+				Timestamp currentTimestamp = new Timestamp(System.currentTimeMillis());
+				currentTimestamp.setNanos(currentTimestamp.getNanos() / 1000 * 1000);
+				System.out.println(currentTimestamp);
+				existingTravelDesk.setAppliedOn(currentTimestamp);
+				existingTravelDesk.setStatus("Pending");
+//				BigInteger bigInteger = new BigInteger(numberString);
+				BigInteger approver = BigInteger.valueOf(21329);
+				existingTravelDesk.setApprover(approver);
+				existingTravelDesk.setLevel(1);
+				existingTravelDesk.setActive(true);
+				TravelDesk savedTravelDesk = tavelDeskRepository.save(existingTravelDesk);
+				if(savedTravelDesk == null) {
+					serviceResponse.setServiceError("Unable to update...!!");
+					serviceResponse.setServiceStatus(ServiceResponse.STATUS_FAIL);
+					return serviceResponse;
+				}
+				else {
+					serviceResponse.setServiceStatus(ServiceResponse.STATUS_SUCCESS);
+					serviceResponse.setServiceResponse(savedTravelDesk);
+					serviceResponse.setServiceMessage("Updated Successfully..!!");
+					return serviceResponse;
+				}
+			}
+			
 		}
 		catch(Exception e) {
 			serviceResponse.setServiceError(e.getMessage());
