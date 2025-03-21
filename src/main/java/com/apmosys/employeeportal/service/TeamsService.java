a56dedc306d23d7f7722472127284e53213e4e3d
prarthanalenka <prarthana.lenka@apmosys.com>
2025-03-21 18:28:04 +0530

    bug 12423

--- src/main/java/com/apmosys/employeeportal/service/TeamsService.java
+++ src/main/java/com/apmosys/employeeportal/service/TeamsService.java
@@ -1201,7 +1201,8 @@ public class TeamsService {
 				
 				// Get Filled EOD Count for Team Members
 				List<Object[]> timesheetList = timesheetsRepository.getMyTeamsFilledEodCountByManagerId(firstOfMonth, end, employeedto.getEmpId());
-
+				System.err.println(timesheetList.size());
+				
 				list.forEach((object) -> {
 					EmployeeDTO dto = new EmployeeDTO();
 					dto.setEmpId(object[0] != null ? Long.parseLong(object[0].toString()): null);
@@ -1226,7 +1227,7 @@ public class TeamsService {
 						dto.setIsDateOfRelievingToday("false");
 					}
 					
-					
+					 dto.setTimesheetStatus("Defaulter");
 					timesheetList.forEach((timesheet) -> {
 
 						Long timesheetEmpId = timesheet[0] != null ? Long.parseLong(timesheet[0].toString()) : null;
@@ -1240,11 +1241,10 @@ public class TeamsService {
 								dto.setTimesheetStatus("Defaulter");
 							}else if (pendingEodCount > 0 && pendingEodCount < 3) {
 								dto.setTimesheetStatus("Pending Timesheets : "+ pendingEodCount);
-							}else {
-								dto.setTimesheetStatus("Timesheets upto date");
 							}
 						}
 					});
+					
 					
 					dtoList.add(dto);
 				});
