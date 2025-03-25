40a79ace29c15151a80f4cb4e6d3a182611f14ff
shivtosh.pal <shivtosh.pal@apmosys.com>
2025-03-25 12:28:52 +0530

    Bug id-12498 by Rahul

--- src/main/frontend/src/app/employee360/employee360-leave/employee360-leave.component.ts
+++ src/main/frontend/src/app/employee360/employee360-leave/employee360-leave.component.ts
@@ -20,9 +20,8 @@ import { Log } from '../../models/log';
 import { LeaveService } from '../../services/leave.service';
 
 import { Sort } from '@angular/material/sort';
-import { ValidationService } from 'src/app/services/validation.service';
-import { async } from 'rxjs';
 import { ExportExcelService } from 'src/app/services/export-excel.service';
+import { ValidationService } from 'src/app/services/validation.service';
 
 declare module 'highcharts' {
   interface Series {
@@ -370,6 +369,7 @@ export class Employee360LeaveComponent implements OnInit {
       (response: any) => {
           if (response.serviceStatus === "Success") {
             this.teamViewLeaveHistoryList=response.serviceResponse;
+
             this.LeaveListOnStatus(this.teamViewLeaveHistoryList);
             //console.log("this.teamViewLeaveHistoryList====>",this.teamViewLeaveHistoryList);
               this.teamViewLeaveHistoryList.forEach((leaveApplication) => {
@@ -432,20 +432,27 @@ export class Employee360LeaveComponent implements OnInit {
 
   LeaveListOnStatus(teamViewLeaveHistoryList: any) {
     this.leaveList = [];
+  
     this.currentUserr=sessionStorage.getItem('currentUser');
     if (this.currentUserr) {
       const currentUserData = JSON.parse(this.currentUserr);
       this.managerId = currentUserData.empId;
-      console.log(this.managerId); 
+     
     }
+  
+   console.log(this.employeeData.empId);
+  
+   this.teamViewLeaveHistoryList = this.teamViewLeaveHistoryList.filter(created => created.createdBy == this.employeeData.empId);
     for (const emp of this.teamViewLeaveHistoryList){
+     
       if(emp.status == this.activeButton){
         if(emp.managerId == this.managerId || emp.level2ApproverId == this.managerId || emp.level3ApproverId == this.managerId){
           emp.isSelected = true;
         }else{
           emp.isSelected = false;
         }
-          this.leaveList.push(emp);
+        this.leaveList.push(emp);
+         
 
       }
 
