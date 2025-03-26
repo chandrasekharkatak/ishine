8f7f286fabdd247b338eb1bd6b6cabc3e8783963
shivtosh.pal <shivtosh.pal@apmosys.com>
2025-03-26 16:45:03 +0530

    Bug id -11671

--- src/main/frontend/src/app/helpers/loader.interceptor.ts
+++ src/main/frontend/src/app/helpers/loader.interceptor.ts
@@ -243,7 +243,8 @@ export class LoaderInterceptor implements HttpInterceptor {
     `${this.baseUrl}` + `api/showAllEmployeeRewards`,
     `${this.baseUrl}` + `api/getAllEmployeesReportByProjectTypeInConsolidated`,
     `${this.baseUrl}`+`api/getAllEmployeesReportByProjectType`,
-     `${this.baseUrl}`+`api/saveAppreciation`
+     `${this.baseUrl}`+`api/saveAppreciation`,
+    `${this.baseUrl}` + `api/deleteCompOff`
   ]
 
   constructor(private loaderService: LoaderService) { }
