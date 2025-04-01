07f4f1d5b8719cfe5c83cfaae5c315f4fe48551b
Priyadarshini Singh <priyadarshini.singh@apmosys.com>
2025-04-01 19:26:21 +0530

    Cron to Update PoProjects With Mails

--- src/main/java/com/apmosys/employeeportal/model/Project.java
+++ src/main/java/com/apmosys/employeeportal/model/Project.java
@@ -13,8 +13,8 @@ import javax.persistence.Table;
 import org.hibernate.envers.Audited;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+//import lombok.Getter;
+//import lombok.Setter;
 import lombok.ToString;
 
 @Entity
