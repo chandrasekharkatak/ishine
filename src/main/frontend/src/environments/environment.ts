5cddcf31c511fe952ff4f62269d81f6905c2fa8e
Priyadarshini Singh <priyadarshini.singh@apmosys.com>
2025-04-08 10:27:30 +0530

    production configuration for deployment

--- src/main/frontend/src/environments/environment.ts
+++ src/main/frontend/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //baseUrl: "https://ishine.apmosys.com/",
-   baseUrl: "http://localhost:8080/",
+  baseUrl: "https://ishine.apmosys.com/",
+  //  baseUrl: "http://localhost:8080/",
   // baseUrl: "http://192.168.21.175:8081/employeeportal/",
 
   lmsbaseurl :"http://192.168.12.113/academy-lms/index.php/",
