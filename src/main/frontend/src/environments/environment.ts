1e1a5b024bac9fec34f65c265d30c25b4d0e46bc
Priyadarshini Singh <priyadarshini.singh@apmosys.com>
2025-04-10 14:11:51 +0530

    uat configurartion

--- src/main/frontend/src/environments/environment.ts
+++ src/main/frontend/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: "https://ishine.apmosys.com/",
-  //  baseUrl: "http://localhost:8080/",
+  // baseUrl: "https://ishine.apmosys.com/",
+   baseUrl: "http://localhost:8080/",
   // baseUrl: "http://192.168.21.175:8081/employeeportal/",
   // baseUrl1: "http://localhost:4200/",
   baseUrl360: "https://ishine.apmosys.com/",
