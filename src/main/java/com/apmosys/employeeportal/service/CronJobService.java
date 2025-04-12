be581e782c7b916cda56ad6b39f30c11b494b5ae
Sakti swarup Das <sakti.das@apmosys.com>
2025-04-12 13:09:13 +0530

    commit for logout

--- src/main/java/com/apmosys/employeeportal/service/CronJobService.java
+++ src/main/java/com/apmosys/employeeportal/service/CronJobService.java
@@ -4536,7 +4536,7 @@ try {
 					    		  userSessionRepository.deleteById(session.getUserSessionId());
 					    	  }else if(elapsedMinsAfterLastCheck >= SESSION_CHECK_INACTIVE_LIMIT){
 					    		  user = session.getEmpId() + " - "+ " Time elapsed After Last Check : "+ elapsedMinsAfterLastCheck + " min.";
-					    		  userSessionRepository.deleteById(session.getUserSessionId());
+//					    		  userSessionRepository.deleteById(session.getUserSessionId());
 					    	  }
 					    	  
 					    	  if(user != null) {
