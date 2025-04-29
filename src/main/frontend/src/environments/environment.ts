9aeb07af6bb3d7fb9d90216390428cef1a3caa1b
Priyadarshini Singh <priyadarshini.singh@apmosys.com>
2025-04-29 18:09:29 +0530

    merge conflict in rmg page, shivtosh's code merged

--- src/main/frontend/src/environments/environment.ts
+++ src/main/frontend/src/environments/environment.ts
@@ -4,12 +4,12 @@
 
 export const environment = {
   production: false,
-  // baseUrl: "https://ishine.apmosys.com/",
-   baseUrl: "http://localhost:8081/",
+  baseUrl: "https://ishine.apmosys.com/",
+  //  baseUrl: "http://localhost:8081/",
   // baseUrl: "http://192.168.21.175:8081/employeeportal/",
 
-  baseUrl360: "http://localhost:4200/",
-  // baseUrl360: "https://ishine.apmosys.com/",
+  // baseUrl360: "http://localhost:4200/",
+  baseUrl360: "https://ishine.apmosys.com/",
   
   lmsbaseurl :"http://192.168.12.113/academy-lms/index.php/",
   //lmsbaseurl :"http://localhost/academy-lms/index.php/",
