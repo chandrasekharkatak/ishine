45da4fe4d8ae590ab0df62cfbfa5ee849c9ad856
Priyadarshini Singh <priyadarshini.singh@apmosys.com>
2025-04-30 09:42:15 +0530

    QR code method changes as per Manoj

--- src/main/frontend/src/environments/environment.ts
+++ src/main/frontend/src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
 
-  baseUrl: "http://localhost:8080/",
+  // baseUrl: "http://localhost:8080/",
   // baseUrl: "http://192.168.21.175:8081/employeeportal/",
-  // baseUrl: "https://ishine.apmosys.com/",
+  baseUrl: "https://ishine.apmosys.com/",
 
-  baseUrl360: "http://localhost:4200/",
-  // baseUrl360: "https://ishine.apmosys.com/",
+  // baseUrl360: "http://localhost:4200/",
+  baseUrl360: "https://ishine.apmosys.com/",
   
   lmsbaseurl :"http://192.168.12.113/academy-lms/index.php/",
   //lmsbaseurl :"http://localhost/academy-lms/index.php/",
