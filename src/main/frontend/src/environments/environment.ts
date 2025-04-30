8c8fd25b8b3688482a3b6bce588c2792e4601bc7
Priyadarshini Singh <priyadarshini.singh@apmosys.com>
2025-04-30 09:55:52 +0530

    changing the configurations to point uat

--- src/main/frontend/src/environments/environment.ts
+++ src/main/frontend/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: "https://ishine.apmosys.com/",
-  //  baseUrl: "http://localhost:8081/",
+  // baseUrl: "https://ishine.apmosys.com/",
+   baseUrl: "http://localhost:8080/",
   // baseUrl: "http://192.168.21.175:8081/employeeportal/",
 
   // baseUrl360: "http://localhost:4200/",
