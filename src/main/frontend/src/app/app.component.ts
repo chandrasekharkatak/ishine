faff1b4fca6113417461d2634ec013c7674e3d29
anyush panda <anyush.panda@apmosys.com>
2025-05-13 16:55:48 +0530

    sky walking agent impl

--- src/main/frontend/src/app/app.component.ts
+++ src/main/frontend/src/app/app.component.ts
@@ -36,40 +36,40 @@ interface SideNavToggle{
   ){
 
     this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
-    // this.router.events.pipe(
-    //   filter(event => event instanceof NavigationEnd)
-    // ).subscribe(() => {
-    //   if (ClientMonitor?.setPerformance) {
-    //     ClientMonitor.setPerformance({
-    //       collector: 'http://192.168.21.175:8081/employeeportal',
-    //       service: 'Ishine::ui',
-    //       serviceVersion: '1.0.0',
-    //       pagePath: location.href,
-    //       useWebVitals: true
-    //     });
-    //   }
-    // });
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      if (ClientMonitor?.setPerformance) {
+        ClientMonitor.setPerformance({
+          collector: 'http://192.168.21.175:8081/employeeportal',
+          service: 'Ishine::ui',
+          serviceVersion: '1.0.0',
+          pagePath: location.href,
+          useWebVitals: true
+        });
+      }
+    });
   }
 
   ngOnInit():void{
-    // import('skywalking-client-js').then(ClientMonitor => {
-    //   console.log('skywalking Client JS loaded:', ClientMonitor);
-    //   if (ClientMonitor.default && typeof ClientMonitor.default.register === 'function') {
-    //     ClientMonitor.default.register({
-    //       service: 'Ishine::ui',
-    //       pagePath: window.location.pathname,
-    //       serviceVersion: '1.0.0',
-    //       useWebVitals:true,
-    //       autoTracePerf:true,
-    //       enableSPA:true,
-    //       collector: "http://192.168.21.175:8081/employeeportal"
-    //     });
-    //     console.log('skywalking initialized successfully');
-    //   } else {
-    //     console.error('skywalking Client JS register function not found.');
-    //   }
-    // }).catch(err => {
-    //   console.error('Error loading skywalking Client JS:', err);    });
+    import('skywalking-client-js').then(ClientMonitor => {
+      console.log('skywalking Client JS loaded:', ClientMonitor);
+      if (ClientMonitor.default && typeof ClientMonitor.default.register === 'function') {
+        ClientMonitor.default.register({
+          service: 'Ishine::ui',
+          pagePath: window.location.pathname,
+          serviceVersion: '1.0.0',
+          useWebVitals:true,
+          autoTracePerf:true,
+          enableSPA:true,
+          collector: "http://192.168.21.175:8081/employeeportal"
+        });
+        console.log('skywalking initialized successfully');
+      } else {
+        console.error('skywalking Client JS register function not found.');
+      }
+    }).catch(err => {
+      console.error('Error loading skywalking Client JS:', err);    });
 
   }
   onToggleSideNav(data: SideNavToggle){
