a7beb7604b0bf2791f013da4194c5cf97b6ccd6f
prarthanalenka <prarthana.lenka@apmosys.com>
2025-05-17 02:26:17 +0530

    deptfilter and approvalstatus filter in employees mapped to ishine,shankh and both

--- src/main/java/com/apmosys/employeeportal/service/ResourceManagementService.java
+++ src/main/java/com/apmosys/employeeportal/service/ResourceManagementService.java
@@ -10,6 +10,7 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -4175,6 +4176,12 @@ public ServiceResponse getTeamInfo(ResourceManagementDTO resourceManagementDTO) 
 		
 		public ServiceResponse getAllInternalProjectsNewRMG(ProjectFilterDTO projectFilterDTO) {
 		    ServiceResponse response = new ServiceResponse();
+		    if ("Not Started".equalsIgnoreCase(projectFilterDTO.getApprovalStatus())) {
+		        response.setServiceStatus(ServiceResponse.STATUS_SUCCESS);
+		        response.setServiceResponse(Collections.emptyList());
+		        return response;
+		    }
+
 		    try {
 		        Employee employee = employeeRepository.findByEmpId(projectFilterDTO.getCurrentUserEmpId());
 		        JobRole jobRole = jobRoleRepository.findByjobRoleId(employee.getJobRoleId());
@@ -4209,6 +4216,12 @@ public ServiceResponse getTeamInfo(ResourceManagementDTO resourceManagementDTO) 
 		        } else {
 		            internalProjectIds = projectRepository.findAllActiveInternalProjectIds();
 		        }
+		        
+		        internalProjectIds = filterProjectidsApprovalStatusDepartmentFilter(
+		                internalProjectIds,
+		                projectFilterDTO.getApprovalStatus(),
+		                projectFilterDTO.getDepartmentsids()
+		            );
 
 		        Map<Long, RMGProjectMappedEmployees> employeeMap = new HashMap<>();
 
@@ -4301,6 +4314,13 @@ public ServiceResponse getTeamInfo(ResourceManagementDTO resourceManagementDTO) 
 		
 		public ServiceResponse getAllShankhProjectsNewRMG(ProjectFilterDTO projectFilterDTO) {
 		    ServiceResponse response = new ServiceResponse();
+		    String approvalStatus = projectFilterDTO.getApprovalStatus();
+		    List<Long> departmentsids = projectFilterDTO.getDepartmentsids();
+		    if ("Not Started".equalsIgnoreCase(projectFilterDTO.getApprovalStatus())) {
+		        response.setServiceStatus(ServiceResponse.STATUS_SUCCESS);
+		        response.setServiceResponse(Collections.emptyList());
+		        return response;
+		    }
 		    try {
 		        Employee employee = employeeRepository.findByEmpId(projectFilterDTO.getCurrentUserEmpId());
 		        JobRole jobRole = jobRoleRepository.findByjobRoleId(employee.getJobRoleId());
@@ -4335,6 +4355,12 @@ public ServiceResponse getTeamInfo(ResourceManagementDTO resourceManagementDTO) 
 		        } else {
 		            internalProjectIds = projectRepository.findAllActiveShankhInternalProjectIds();
 		        }
+		        
+		        internalProjectIds = filterProjectidsApprovalStatusDepartmentFilter(
+		                internalProjectIds,
+		                projectFilterDTO.getApprovalStatus(),
+		                projectFilterDTO.getDepartmentsids()
+		            );
 
 		        Map<Long, RMGProjectMappedEmployees> employeeMap = new HashMap<>();
 
@@ -4427,6 +4453,12 @@ public ServiceResponse getTeamInfo(ResourceManagementDTO resourceManagementDTO) 
 		
 		public ServiceResponse getAllShankhInternalProjectsNewRMG(ProjectFilterDTO projectFilterDTO) {
 		    ServiceResponse response = new ServiceResponse();
+		    
+		    if ("Not Started".equalsIgnoreCase(projectFilterDTO.getApprovalStatus())) {
+		        response.setServiceStatus(ServiceResponse.STATUS_SUCCESS);
+		        response.setServiceResponse(Collections.emptyList());
+		        return response;
+		    }
 		    try {
 		        Employee employee = employeeRepository.findByEmpId(projectFilterDTO.getCurrentUserEmpId());
 		        JobRole jobRole = jobRoleRepository.findByjobRoleId(employee.getJobRoleId());
@@ -4461,6 +4493,12 @@ public ServiceResponse getTeamInfo(ResourceManagementDTO resourceManagementDTO) 
 		        } else {
 		            internalProjectIds = projectRepository.findAllActiveShankhProjectIds();
 		        }
+		        
+		        internalProjectIds = filterProjectidsApprovalStatusDepartmentFilter(
+		                internalProjectIds,
+		                projectFilterDTO.getApprovalStatus(),
+		                projectFilterDTO.getDepartmentsids()
+		            );
 
 		        Map<Long, RMGProjectMappedEmployees> employeeMap = new HashMap<>();
 
@@ -4550,5 +4588,67 @@ public ServiceResponse getTeamInfo(ResourceManagementDTO resourceManagementDTO) 
 		    }
 		    return response;
 		}
+		
+		public Set<Integer> filterProjectidsApprovalStatusDepartmentFilter(
+		        Set<Integer> projectIds,
+		        String approvalStatus,
+		        List<Long> departmentsids) {
+
+		    Set<Integer> filteredProjectIds = new HashSet<>();
+
+		    try {
+		        List<Object[]> allProjectList = projectRepository.findAllProjectByIsDraftAndIsActiveOfProjectIds(projectIds);
+
+		        if (!allProjectList.isEmpty()) {
+		            List<ResourceManagementDTO> dtoList = new ArrayList<>();
+
+		            for (Object[] object : allProjectList) {
+		                Integer projectId = object[0] != null ? Integer.parseInt(object[0].toString()) : null;
+		                String isDraftProject = object[2] != null ? object[2].toString() : null;
+		                Long isActive = object[3] != null ? Long.parseLong(object[3].toString()) : null;
+
+		               
+		                boolean matchesApproval = false;
+
+		                if ("Pending For Approval".equalsIgnoreCase(approvalStatus) && Long.valueOf(2).equals(isActive)) {
+		                    matchesApproval = true;
+		                } else if ("Approved".equalsIgnoreCase(approvalStatus) && "false".equalsIgnoreCase(isDraftProject)) {
+		                    matchesApproval = true;
+		                } else if ("Rejected".equalsIgnoreCase(approvalStatus) && "Rejected".equalsIgnoreCase(isDraftProject)) {
+		                    matchesApproval = true;
+		                }else if("All".equalsIgnoreCase(approvalStatus)) {
+		                	 matchesApproval = true;
+		                }
+
+		                if (!matchesApproval) continue;
+
+		                
+		                List<TeamSpocDTO> spocList = getTeamSpocsByProjectId(projectId);
+
+		               
+		                boolean matchesDepartment = true;
+		                if (departmentsids != null && !departmentsids.isEmpty()) {
+		                    matchesDepartment = spocList.stream().anyMatch(spoc ->
+		                        spoc.getDepartmentList() != null &&
+		                        Arrays.stream(spoc.getDepartmentList())
+		                              .map(Long::valueOf)
+		                              .anyMatch(departmentsids::contains)
+		                    );
+		                }
+
+		                
+		                if (matchesDepartment) {
+		                    filteredProjectIds.add(projectId);
+		                }
+		            }
+		        }
+
+		    } catch (Exception e) {
+		        e.printStackTrace();
+		    }
+
+		    return filteredProjectIds;
+		}
+
 
 }
