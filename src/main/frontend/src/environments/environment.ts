3897ffb252e99ea9eeacd181aa5cdbc48af7d75f
Priyadarshini Singh <priyadarshini.singh@apmosys.com>
2025-06-16 14:03:14 +0530

    local url default

--- src/main/frontend/src/environments/environment.ts
+++ src/main/frontend/src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   // baseUrl: "https://ishine.apmosys.com/",
-    // baseUrl: "http://localhost:8080/",
-    baseUrl: "http://192.168.21.175:8081/employeeportal/",
+    baseUrl: "http://localhost:8080/",
+    // baseUrl: "http://192.168.21.175:8081/employeeportal/",
 
-  //  baseUrl360: "http://localhost:4200/",
+   baseUrl360: "http://localhost:4200/",
   // baseUrl360: "https://ishine.apmosys.com/",
-   baseUrl360: "http://192.168.21.175:8081/employeeportal/",
+  //  baseUrl360: "http://192.168.21.175:8081/employeeportal/",
   
   lmsbaseurl :"http://192.168.12.113/academy-lms/index.php/",
   //lmsbaseurl :"http://localhost/academy-lms/index.php/",
