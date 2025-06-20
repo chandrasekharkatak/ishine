fd84254a2da121ab25386aee4f2cd853b1476def
shivtosh.pal <shivtosh.pal@apmosys.com>
2025-06-20 15:51:59 +0530

    Minor change in JPQL QUERY

--- src/main/java/com/apmosys/employeeportal/repository/DepartmentRepository.java
+++ src/main/java/com/apmosys/employeeportal/repository/DepartmentRepository.java
@@ -52,7 +52,7 @@ public interface DepartmentRepository extends JpaRepository<Department, Long> {
 	@Query(nativeQuery = true , value = "select * from department where name = :deptname")
 	public List<Department> findByDeptName(String deptname);
 	
-	@Query(value = "select d.deptId from Department where hodId=:hodId")
+	@Query(value = "select d.deptId from Department d where d.hodId=:hodId")
 	 List<Long> findDeptIdsByHodId(@Param("hodId") Long hodId);
 	
 	@Query(nativeQuery = true,value ="select dept_id from job_role where job_role_id = :jobRoleId")
