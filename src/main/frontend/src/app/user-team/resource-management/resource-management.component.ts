30442592e023708ce2dcf7904697a888643d679d
Priyadarshini Singh <priyadarshini.singh@apmosys.com>
2025-06-21 17:40:32 +0530

    project status reallignmenet

--- src/main/frontend/src/app/user-team/resource-management/resource-management.component.ts
+++ src/main/frontend/src/app/user-team/resource-management/resource-management.component.ts
@@ -185,8 +185,8 @@ export class ResourceManagementComponent implements OnInit {
   employeeRole: any[] = ['Employee', 'TeamLead', 'Manager', 'HOD', 'HR', 'SuperAdmin', 'RMG'];
   filters: any = {};
   isSearchEnabled: boolean = false;
-  projectColumns: any[] = ["blank", "draftStatus", "name", "poNo", "projectType", "projectManagerName", "clientName", "apmosysRM", "clientRM", "poStartDate", "poEndDate", "state", "createdOn", "status"];
-
+  // projectColumns: any[] = ["blank", "draftStatus", "name", "poNo", "projectType", "projectManagerName", "clientName", "apmosysRM", "clientRM", "poStartDate", "poEndDate", "state", "createdOn", "status"];
+  projectColumns: any[] = ["blank", "name", "poNo", "projectType", "projectManagerName", "clientName", "apmosysRM", "clientRM", "poStartDate", "poEndDate", "state", "createdOn", "status", "draftStatus"];
 
   projectDetails: any = [];
   copyDepartment: any = [];
