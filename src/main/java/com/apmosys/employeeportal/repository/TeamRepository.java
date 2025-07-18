4624c69437dce4377defec941c4d47fe66635f32
Shubhank Nagar <shubhanknagar@gmail.com>
2025-07-18 15:18:01 +0530

    changes in resource management

--- src/main/java/com/apmosys/employeeportal/repository/TeamRepository.java
+++ src/main/java/com/apmosys/employeeportal/repository/TeamRepository.java
@@ -137,4 +137,7 @@ public interface TeamRepository extends JpaRepository<Team, Long>{
 	public List<Team> findByTeamLeadIdAndIsActive(Long teamLeadId, String isActive);
 
 	public List<Team> findBySpocIdAndIsActive(Long spocId, String string);
+	
+	@Query(nativeQuery = true, value = "select * from teams t inner join projects p on p.project_id = t.project_id where p.po_project_id = :id and t.is_active = \"Y\"")
+	public List<Team> findTeamandIsActive(@Param("id") Long id);
 }
