ee973a58219a336aff93a6f2dba8eddd41149d09
Priyadarshini Singh <priyadarshini.singh@apmosys.com>
2025-08-01 10:58:19 +0530

    retracking to previous way of data call

--- src/main/frontend/src/app/project-view/project-view.component.ts
+++ src/main/frontend/src/app/project-view/project-view.component.ts
@@ -99,27 +99,27 @@ export class ProjectViewComponent implements OnInit {
   ) { this.authenticationService.currentUser.subscribe(x => this.currentUser = x); }
 
   ngOnInit(): void {
-    // const storedData = localStorage.getItem('projectId');
-    // const parsedData = storedData || null;
+    const storedData = localStorage.getItem('projectId');
+    const parsedData = storedData || null;
 
-    // console.log("storedData ", storedData);
-    // console.log("parsedData ", parsedData);
+    console.log("storedData ", storedData);
+    console.log("parsedData ", parsedData);
 
-    // if (parsedData !== null && parsedData !== undefined) {
-    //   this.selectedProjectId = parsedData;
-    // } else {
-    //   this.selectedProjectId = history.state.data;
-    // }
+    if (parsedData !== null && parsedData !== undefined) {
+      this.selectedProjectId = parsedData;
+    } else {
+      this.selectedProjectId = history.state.data;
+    }
 
-    this.route.queryParams.subscribe(params => {
-      this.selectedProjectId = params['projectId'];
-      console.log('Received projectId from query param:', this.selectedProjectId);
-    if (this.selectedProjectId) {
-        this.getTeamInfo(this.selectedProjectId);
-      } else {
-        console.warn("projectId is missing in query params.");
-      }
-    });
+    // this.route.queryParams.subscribe(params => {
+    //   this.selectedProjectId = params['projectId'];
+    //   console.log('Received projectId from query param:', this.selectedProjectId);
+    // if (this.selectedProjectId) {
+    //     this.getTeamInfo(this.selectedProjectId);
+    //   } else {
+    //     console.warn("projectId is missing in query params.");
+    //   }
+    // });
     this.getProjectInfo();
   }
 
