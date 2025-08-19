313fe0f3a2192ede233d1320f0805bf3abca3030
Priyadarshini Singh <priyadarshini.singh@apmosys.com>
2025-08-19 15:39:59 +0530

    Setting deptName in Employees not mapped to any project

--- src/main/frontend/src/app/user-team/resource-management/view-employee/view-employee.component.ts
+++ src/main/frontend/src/app/user-team/resource-management/view-employee/view-employee.component.ts
@@ -490,7 +490,7 @@ exportNotMappedToExcel(): void {
       "Sr No.": index + 1,
       "Employment Id": "A-" + employee.employeementId,
       "Employee Name": employee.name,
-      "Department": employee.departmentName,
+      "Department": employee.deptName,
       "Billable Type": employee.billableType,
       "Manager Name": employee.managerName,
       "Employee Role": employee.jobRoleName
