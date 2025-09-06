9341d2ef07f2c200ca330264a97bf8e359b1dc93
Sakti swarup Das <sakti.das@apmosys.com>
2025-09-06 16:05:20 +0530

    1.Table design changes UI Fix

--- src/main/frontend/src/app/user-report/report-list/report-list.component.ts
+++ src/main/frontend/src/app/user-report/report-list/report-list.component.ts
@@ -253,7 +253,7 @@ ActivePoCounts: {
 dateRange: string; type: string; count: string;
 }[] = [];
   visibleInfo1: boolean = false;
-
+ expandedClients: { [key: string]: boolean } = {};
 
   constructor(
     private authenticationService: AuthenticationService,
@@ -3518,11 +3518,10 @@ exportModalDataToExcel() {
     'Created On': project.createdOn
   }));
 }
-expandedClients: { [clientName: string]: boolean } = {};
 
-toggleClientDetails(clientName: string) {
-  this.expandedClients[clientName] = !this.expandedClients[clientName];
-}
+// toggleClientDetails(clientName: string) {
+//   this.expandedClients[clientName] = !this.expandedClients[clientName];
+// }
 
 expandedDepartments: { [key: string]: boolean } = {};
 
@@ -3541,6 +3540,9 @@ getTotalActiveEmployees(clientGroup: any): number {
   return total;
 }
 
+  toggleClientRow(clientName: string): void {
+    this.expandedClients[clientName] = !this.expandedClients[clientName];
+  }
 
 }
 
