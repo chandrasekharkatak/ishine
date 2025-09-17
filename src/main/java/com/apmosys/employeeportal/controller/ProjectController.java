d51552120f901b71f60cebda6befadbaf7f80f07
shivtosh.pal <shivtosh.pal@apmosys.com>
2025-09-17 17:56:32 +0530

    Applying encryption on Employee related api and project Api

--- src/main/java/com/apmosys/employeeportal/controller/ProjectController.java
+++ src/main/java/com/apmosys/employeeportal/controller/ProjectController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.apmosys.employeeportal.Encrypted;
 import com.apmosys.employeeportal.dto.ClientProjectReportDTO;
 import com.apmosys.employeeportal.dto.GetEmployeeProjectReportPayloadDTO;
 import com.apmosys.employeeportal.dto.GetProjectToEmployeeReportForProjectDTO;
@@ -36,70 +37,70 @@ public class ProjectController {
 	
 	@Autowired
 	EmployeeService employeeService;
-	
+	@Encrypted
 	@RequestMapping(value = "/getAllClients", method = RequestMethod.GET)
 	public ServiceResponse getAllClients() {
 		
 		ServiceResponse response = projectService.getAllClients();
 		return response;
 	}
-	
+	@Encrypted
 	@RequestMapping(value = "/getAllProjects", method = RequestMethod.GET)
 	public ServiceResponse getAllProjects() {
 		
 		ServiceResponse response = projectService.getAllProjects();
 		return response;
 	}
-	
+	@Encrypted
 	@RequestMapping(value = "/createProject", method = RequestMethod.POST)
 	public ServiceResponse createProject(@RequestBody PoProjectSyncDTO poProjectSyncDto) {
 		employeeService.clearEmployeeCache();
 		ServiceResponse response = projectService.createProject(poProjectSyncDto);
 		return response;
 	}
-	
+	@Encrypted
 	@RequestMapping(value = "/getProjectByProjectId", method = RequestMethod.POST)
 	public ServiceResponse getProjectByProjectId(@RequestBody PoProjectSyncDTO poProjectSyncDto) {
 		
 		ServiceResponse response = projectService.getProjectByProjectId(poProjectSyncDto);
 		return response;
 	}
-	
+	@Encrypted
 	@RequestMapping(value = "/updateProject", method = RequestMethod.POST)
 	public ServiceResponse updateProject(@RequestBody PoProjectSyncDTO poProjectSyncDto) {
 		employeeService.clearEmployeeCache();
 		ServiceResponse response = projectService.updateProject(poProjectSyncDto);
 		return response;
 	}
-	
+	@Encrypted
 	@RequestMapping(value = "/deleteProject", method = RequestMethod.POST)
 	public ServiceResponse deleteProject(@RequestBody PoProjectSyncDTO poProjectSyncDto) {
 		employeeService.clearEmployeeCache();
 		ServiceResponse response = projectService.deleteProject(poProjectSyncDto);
 		return response;
 	}
-	
+	@Encrypted
 	@RequestMapping(value = "/syncPoProjectAndTeam", method = RequestMethod.POST)
 	public ServiceResponse syncPoProjectAndTeam(@RequestBody PoProjectSyncDTO[] poProjectSyncDto) {
 		
 		ServiceResponse response = projectService.syncPoProjectAndTeam(poProjectSyncDto);
 		return response;
 	}
-	
+	@Encrypted
 	@RequestMapping(value = "/getSyncableProject", method = RequestMethod.GET)
 	public ServiceResponse getSyncableProject() {
 		
 		ServiceResponse response = projectService.getSyncableProject();
 		return response;
 	}
-	
+	@Encrypted
 	@RequestMapping(value = "/checkProjectName", method = RequestMethod.POST)
 	public ServiceResponse checkProjectName(@RequestBody ProjectDTO projectDto) {
 		
 		ServiceResponse response = projectService.checkProjectName(projectDto);
 		return response;
 	}
-	
+	@Encrypted
 	@RequestMapping(value = "/getAllMyProjectByEmpId", method = RequestMethod.POST)
 	public ServiceResponse getAllMyProjectByEmpId(@RequestBody ProjectDTO projectDto) {
 		
@@ -111,46 +112,46 @@ public class ProjectController {
 	public ResponseEntity<ServiceResponse> poprojectclone() {
 	    return ResponseEntity.ok(projectService.getProjectCloneFromPoPortal());
 	}
-	
+	@Encrypted
 	@PostMapping(value = "/poProjectTimesheetSync")
 	public ServiceResponse poProjectTimesheetSync(@RequestBody Set<Long> projectIdList) {
 		return projectService.poProjectTimesheetSync(projectIdList);
 	}
-	
+	@Encrypted
 	@PostMapping(value = "/getEmployeeProjectReport")
 	public ServiceResponse getEmployeeProjectReport(@RequestBody GetEmployeeProjectReportPayloadDTO dto) {
 		return projectService.getEmployeeProjectReport(dto);
 	}
-	
+	@Encrypted
 	@GetMapping(value = "/getResourceRequirementFromPoPortal")
 	public ServiceResponse getResourceRequirementFromPoPortal() {
 		return projectService.getResourceRequirementFromPoPortal();
 	}
 	
-
+	@Encrypted
 	@PostMapping(value = "/getProjectWithCliendSideID")
 	public ServiceResponse getProjectWithCliendSideID(@RequestBody ProjectDTO projectDto) {
 		return projectService.getProjectWithCliendSideID(projectDto);
 	}
-	
+	@Encrypted
 	@PostMapping(value="/getCompletedFixedCostProjects")
 	public ServiceResponse getCompletedFixedCostProjects(@RequestBody ProjectRequest projectRequest){
 		return projectService.getCompletedFixedCostProjects(projectRequest);
 	}
-	
+	@Encrypted
 	@PostMapping(value = "/getFixedCostCount")
 	public ServiceResponse getFixedCostCount(@RequestBody ProjectFilterDTO projectFilter) {
 		return projectService.getFcCount(projectFilter);
 	}
 	
 
-
+@Encrypted
 @RequestMapping(value = "/getClientAndProjectReport", method = RequestMethod.POST)
 public ServiceResponse getClientAndProjectReport(@RequestBody ClientProjectReportDTO clientProjectReportDTO) {
     ServiceResponse response = projectService.getClientAndProjectReport(clientProjectReportDTO);
     return response;
 }
-
+@Encrypted
 @RequestMapping(value = "/getClientAndProjectReportDataList", method = RequestMethod.POST)
 public ServiceResponse getClientAndProjectReportDataList(@RequestBody ClientProjectReportDTO clientProjectReportDTO) {
     ServiceResponse response = projectService.getClientAndProjectReportDataList(clientProjectReportDTO);
