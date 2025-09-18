b6f0b9903b2f7c7180d7c681fcc7e62e3b055fe4
shivtosh.pal <shivtosh.pal@apmosys.com>
2025-09-18 10:58:02 +0530

    Applied for revoked Condition in leave VAPT points

--- src/main/java/com/apmosys/employeeportal/repository/EmployeeLeaveRepository.java
+++ src/main/java/com/apmosys/employeeportal/repository/EmployeeLeaveRepository.java
@@ -198,7 +198,7 @@ List<Object[]> reportingManagerIsOnLeave(@Param("empId") Long empId);
 
 @Query("SELECT COUNT(e) > 0 FROM EmployeeLeave e " +
 	       "WHERE e.empId = :empId " +
-	       "AND e.leaveStatusId IN (1, 2) " +
+	       "AND e.leaveStatusId IN (1,2,4) " +
 	       "AND e.fromDate <= :toDate " +
 	       "AND e.toDate >= :fromDate")
 	boolean existsOverlappingLeave(@Param("empId") Long empId,
