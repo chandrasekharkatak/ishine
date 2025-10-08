460366fa24e35aee5173d0b623b40a660f086cde
Priyadarshini Singh <priyadarshini.singh@apmosys.com>
2025-10-08 19:39:19 +0530

    totalIshineFilledCount removed from custom filter and export

--- src/main/frontend/src/app/user-timesheet/hr-dashboard/hr-dashboard.component.ts
+++ src/main/frontend/src/app/user-timesheet/hr-dashboard/hr-dashboard.component.ts
@@ -124,7 +124,7 @@ export class HrDashboardComponent implements AfterViewInit {
   docData: any;
   mimeType: any;
   projectView:ProjectViewForTimesheet[] = [];
-  projectViewColumns: any[] = ['projectName','poNo','projectManagerName','projectType','clientName','apmosysRM','apmosysRMEmail','clientRM','totalExpectedFillCount','totalIshineFilledCount','totalClientSideApprovedCount','clientSideApprovedPercent','totalClientSidePendingCount','clientSidePendingPercent','totalClientSideNotFilledCount','clientSideNotFilledPercent'];
+  projectViewColumns: any[] = ['projectName','poNo','projectManagerName','projectType','clientName','apmosysRM','apmosysRMEmail','clientRM','totalExpectedFillCount','totalClientSideApprovedCount','clientSideApprovedPercent','totalClientSidePendingCount','clientSidePendingPercent','totalClientSideNotFilledCount','clientSideNotFilledPercent'];
   timesheetSummaryColumns:any[]=['blank','employeementId','employeeName','projectName','expectedEODCount','submittedCount','clientApprovedCount','clientPendingCount'];
   totalClientSideApprovedCount: any;
   eodNotFilledCount: any;
@@ -838,7 +838,7 @@ employeeListAccordingToProject:any[]=[];
       'Email': x.email || 'NA',
       'Department': x.departmentName || 'NA',
       'Expected DSR': x.expectedFillCount ?? 0,
-      'Ishine DSR': x.timesheetFilledCount ?? 0,
+      // 'Ishine DSR': x.timesheetFilledCount ?? 0,
       'Client Filled': x.clientSideAttendancePendingCount ?? 0,
       'Client Approved': x.clientSideAttendanceApprovedCount ?? 0,
       'Client Not Filled': x.clientSideAttendanceNotFilledCount ?? 0,
@@ -1158,7 +1158,7 @@ console.log("Hiii");
       'Apmosys RM Email': project.apmosysRMEmail || 'NA',
       'Client RM': project.clientRM || 'NA',
       'Expected Fill Count': project.totalExpectedFillCount ?? 0,
-      'iShine Filled Count': project.totalIshineFilledCount ?? 0,
+      // 'iShine Filled Count': project.totalIshineFilledCount ?? 0,
       'Client Pending %': (project.clientSidePendingPercent ?? 0) + '%',
       'Client Side Pending': project.totalClientSidePendingCount ?? 0,
       'Client Approved %': (project.clientSideApprovedPercent ?? 0) + '%',
