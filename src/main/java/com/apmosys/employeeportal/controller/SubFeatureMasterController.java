5bd4e8a9fe54ad90b9f996258c2ee46bc67931cc
nabarun.samanta@apmosys.com <nabarun.samanta@apmosys.com>
2025-10-24 18:49:09 +0530

    adding job based access

--- src/main/java/com/apmosys/employeeportal/controller/SubFeatureMasterController.java
+++ src/main/java/com/apmosys/employeeportal/controller/SubFeatureMasterController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.apmosys.employeeportal.JobRoleAccess;
 import com.apmosys.employeeportal.dto.JobRoleDTO;
 import com.apmosys.employeeportal.service.SubFeatureMasterService;
 import com.apmosys.employeeportal.utility.ServiceResponse;
@@ -17,14 +18,14 @@ public class SubFeatureMasterController {
 	@Autowired
 	SubFeatureMasterService subFeatureMasterService;
 	
-	
+	@JobRoleAccess(featureIds = {5})
 	@RequestMapping(value = "/getSubfeaturesByJobRoleId" ,method = RequestMethod.POST)
 	public ServiceResponse getSubfeaturesByJobRoleId(@RequestBody JobRoleDTO jobRoleDTO) {
 		
 		ServiceResponse response = subFeatureMasterService.getSubfeaturesByJobRoleId(jobRoleDTO);
 		return response;
 	}
-	
+	@JobRoleAccess(featureIds = {5})
 	@RequestMapping(value="/getAllSubFeatures" , method = RequestMethod.GET)
 	public ServiceResponse getAllSubFeatures() {		
 		
