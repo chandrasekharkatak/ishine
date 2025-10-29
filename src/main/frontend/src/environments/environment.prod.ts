006428e3b13ab3451ef634a229dd6c1123c51d4b
Priyadarshini Singh <priyadarshini.singh@apmosys.com>
2025-10-29 19:28:51 +0530

    qrCodebaseUrl updated

--- src/main/frontend/src/environments/environment.prod.ts
+++ src/main/frontend/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   baseUrl: "./",
   lmsbaseurl :"./",
-  qrCodebaseUrl: "http://103.114.154.240:8081/qrcodegenerator/#/main",
+  qrCodebaseUrl: "http://103.114.154.240:8081/qrcodegenerator/#/",
   baseUrl360:"./"
 };
