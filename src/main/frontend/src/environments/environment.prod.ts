8796c9e894fd8f09644c0b2d26e04efb23e631e2
Sakti swarup Das <sakti.das@apmosys.com>
2025-11-03 10:44:14 +0530

    1.Environment Prod Ts changes for QR Code URL

--- src/main/frontend/src/environments/environment.prod.ts
+++ src/main/frontend/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   baseUrl: "./",
   lmsbaseurl :"./",
-  qrCodebaseUrl: "https://ishine.apmosys.com/qrcodegenerator/",
+  qrCodebaseUrl: "https://ishine.apmosys.com/qrcodegenerator/#/",
   baseUrl360:"./"
 };
