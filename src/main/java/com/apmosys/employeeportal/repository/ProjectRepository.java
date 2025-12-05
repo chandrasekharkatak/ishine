12045c40bc39fca2290a1c4365435654907b98d9
soumyadeep <soumyadeepghosh126@gmail.com>
2025-12-05 19:40:42 +0530

    1.minor fix

--- src/main/java/com/apmosys/employeeportal/repository/ProjectRepository.java
+++ src/main/java/com/apmosys/employeeportal/repository/ProjectRepository.java
@@ -4433,7 +4433,7 @@ boolean existsByProjectName(String projectName);
 				            int offset,int pageSize
 				    );
 				    
-				    @Query(value="select p.client_flag from projects p  where p.project_id = :projectId",nativeQuery = true)
+				    @Query(value="select COALESCE(p.client_flag, false) from projects p  where p.project_id = :projectId",nativeQuery = true)
 				    public boolean isClientIdMandetory(@Param("projectId")int projectId);
 				    
 				    
