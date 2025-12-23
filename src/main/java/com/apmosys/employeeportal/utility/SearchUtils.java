805abe4604334ba80dd9039ac61dbe8f688642bf
Tushar Shinde <tushar.shinde@apmosys.com>
2025-12-23 16:44:22 +0530

    Not required Dependencies removed

--- src/main/java/com/apmosys/employeeportal/utility/SearchUtils.java
+++ src/main/java/com/apmosys/employeeportal/utility/SearchUtils.java
@@ -9,41 +9,41 @@ import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import com.meilisearch.sdk.Client;
-import com.meilisearch.sdk.SearchRequest;
+// import com.meilisearch.sdk.Client;
+// import com.meilisearch.sdk.SearchRequest;
 
 @Service
 public class SearchUtils {
 
-    private final Client meiliSearchClient;
-    private final ObjectMapper objectMapper;
+    // private final Client meiliSearchClient;
+    // private final ObjectMapper objectMapper;
 
-    @Autowired
-    public SearchUtils(Client meiliSearchClient, ObjectMapper objectMapper) {
-        this.meiliSearchClient = meiliSearchClient;
-        this.objectMapper = objectMapper;
-    }
+    // @Autowired
+    // public SearchUtils(Client meiliSearchClient, ObjectMapper objectMapper) {
+    //     this.meiliSearchClient = meiliSearchClient;
+    //     this.objectMapper = objectMapper;
+    // }
 
-    public void addData(String indexName, List<Map<String, Object>> data) {
-        try {
-            String json = objectMapper.writeValueAsString(data);
-            meiliSearchClient.index(indexName).addDocuments(json);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    // public void addData(String indexName, List<Map<String, Object>> data) {
+    //     try {
+    //         String json = objectMapper.writeValueAsString(data);
+    //         meiliSearchClient.index(indexName).addDocuments(json);
+    //     } catch (Exception e) {
+    //         e.printStackTrace();
+    //     }
+    // }
     
-    public Object searchData(String indexName, String query) {
-        try {
-            SearchRequest searchRequest = new SearchRequest(query)
-            .setAttributesToSearchOn(new String[]{"tag"})
-            .setLimit(10);
+    // public Object searchData(String indexName, String query) {
+    //     try {
+    //         SearchRequest searchRequest = new SearchRequest(query)
+    //         .setAttributesToSearchOn(new String[]{"tag"})
+    //         .setLimit(10);
 
-            return meiliSearchClient.index(indexName).search(searchRequest);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+    //         return meiliSearchClient.index(indexName).search(searchRequest);
+    //     } catch (Exception e) {
+    //         e.printStackTrace();
+    //     }
+    //     return null;
+    // }
     
 }
--- src/main/java/com/apmosys/employeeportal/utility/TagUtils.java
+++ src/main/java/com/apmosys/employeeportal/utility/TagUtils.java
@@ -20,136 +20,140 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.apmosys.employeeportal.config.MeilisearchConfig;
+// import com.apmosys.employeeportal.config.MeilisearchConfig;
 import com.apmosys.employeeportal.dto.TagDTO;
 import com.apmosys.employeeportal.model.TagMaster;
 import com.apmosys.employeeportal.repository.TagMasterRepository;
 
 @Component
 public class TagUtils {
-	
+
 	@Value("${pythonUrl.for.ocr}")
 	private String pythonUrl;
-	
+
 	@Autowired
 	NLPUtils nlpUtils;
-	
+
 	@Autowired
 	TagMasterRepository tagMasterRepository;
 
-	@Autowired
-	private SearchUtils searchUtils;
-	
-	//save tags project-milestone-module-submodule wise
-	public ServiceResponse saveTags(TagDTO tagDTO) {
-		ServiceResponse response = new ServiceResponse();
-		try {
-	        String projectText = tagDTO.getProjectText().toString();
+	// @Autowired
+	// private SearchUtils searchUtils;
 
-	        if (tagDTO.getProjectId() == null || projectText == null || projectText.isEmpty()) {
-	        	response.setServiceMessage("Invalid Project ID or Text or TagType");
-	        	response.setServiceStatus(ServiceResponse.STATUS_FAIL);
-	        	return response;
-	        }
+	// //save tags project-milestone-module-submodule wise
+	// public ServiceResponse saveTags(TagDTO tagDTO) {
+	// ServiceResponse response = new ServiceResponse();
+	// try {
+	// String projectText = tagDTO.getProjectText().toString();
 
-	        List<String> extractedTags = nlpUtils.extractTags(projectText);
-	        List<TagMaster> existingTags = tagMasterRepository.findByEntityIdAndEntityType(tagDTO.getEntityId(), tagDTO.getEntityType());
-	        Set<String> existingTagNames = existingTags.stream()
-	        											.map(TagMaster::getTag)
-	        											.collect(Collectors.toSet());
+	// if (tagDTO.getProjectId() == null || projectText == null ||
+	// projectText.isEmpty()) {
+	// response.setServiceMessage("Invalid Project ID or Text or TagType");
+	// response.setServiceStatus(ServiceResponse.STATUS_FAIL);
+	// return response;
+	// }
 
-	        if ("response".equalsIgnoreCase(tagDTO.getTagType())) {
-	            extractedTags.removeIf(existingTagNames::contains);
-	        } else {
-	            tagMasterRepository.deleteAll(existingTags);
-	        }
+	// List<String> extractedTags = nlpUtils.extractTags(projectText);
+	// List<TagMaster> existingTags =
+	// tagMasterRepository.findByEntityIdAndEntityType(tagDTO.getEntityId(),
+	// tagDTO.getEntityType());
+	// Set<String> existingTagNames = existingTags.stream()
+	// .map(TagMaster::getTag)
+	// .collect(Collectors.toSet());
 
-	        // Save new tags if any remain
-	        if (!extractedTags.isEmpty()) {
-				List<Map<String, Object>> meiliDataList = new ArrayList<>();
-				List<TagMaster> newTags = extractedTags.stream()
-				.filter(tag -> tag != null)
-				.map(tag -> {
-					TagMaster tm = new TagMaster(); 	                    
-					tm.setEntityId(tagDTO.getEntityId());
-					tm.setEntityType(tagDTO.getEntityType());
-					tm.setProjectId(tagDTO.getProjectId());
-					tm.setTag(tag);
-					tm.setType(tagDTO.getType());
-					
-						Map<String, Object> meiliData = new HashMap<>();
-						meiliData.put("tag", tag);
-						meiliData.put("projectId", tagDTO.getProjectId());
+	// if ("response".equalsIgnoreCase(tagDTO.getTagType())) {
+	// extractedTags.removeIf(existingTagNames::contains);
+	// } else {
+	// tagMasterRepository.deleteAll(existingTags);
+	// }
 
-						meiliDataList.add(meiliData);
- 	                    
- 	                    return tm;
-	                })
-	                .collect(Collectors.toList());
+	// // Save new tags if any remain
+	// if (!extractedTags.isEmpty()) {
+	// List<Map<String, Object>> meiliDataList = new ArrayList<>();
+	// List<TagMaster> newTags = extractedTags.stream()
+	// .filter(tag -> tag != null)
+	// .map(tag -> {
+	// TagMaster tm = new TagMaster();
+	// tm.setEntityId(tagDTO.getEntityId());
+	// tm.setEntityType(tagDTO.getEntityType());
+	// tm.setProjectId(tagDTO.getProjectId());
+	// tm.setTag(tag);
+	// tm.setType(tagDTO.getType());
 
-	            List<TagMaster> savedTags = tagMasterRepository.saveAll(newTags);
+	// Map<String, Object> meiliData = new HashMap<>();
+	// meiliData.put("tag", tag);
+	// meiliData.put("projectId", tagDTO.getProjectId());
 
-				if(!meiliDataList.isEmpty()){
-					searchUtils.addData("tags", meiliDataList);
-				}
+	// meiliDataList.add(meiliData);
 
-	            if (!savedTags.isEmpty()) {
-	            	response.setServiceMessage("Success");
-		        	response.setServiceStatus(ServiceResponse.STATUS_SUCCESS);
-	            }else {
-	            	response.setServiceMessage("Failed");
-		        	response.setServiceStatus(ServiceResponse.STATUS_FAIL);
-	            }
-	        } else {
-	        	response.setServiceMessage("No New Tags to Save");
-	        	response.setServiceStatus(ServiceResponse.STATUS_SUCCESS);
-	        }
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		return response;
-	}
-	
-	
-	//Share file (pdf , image, excel, word etc) to python for OCR
-	public ServiceResponse sendFileForOCR(MultipartFile document, Long entityId, String entityName, Long questionMasterId, Long projectId) {
-		ServiceResponse response = new ServiceResponse();
-		try {
-			if (document != null) {
-			    String dmsPortalUploadUrl = pythonUrl + "/upload";
-			    RestTemplate restTemplate = new RestTemplate();
+	// return tm;
+	// })
+	// .collect(Collectors.toList());
 
-			    HttpHeaders headers = new HttpHeaders();
-			    headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+	// List<TagMaster> savedTags = tagMasterRepository.saveAll(newTags);
 
-			    MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
-			    body.add("file", new ByteArrayResource(document.getBytes()) {
-			        @Override
-			        public String getFilename() {
-			            return document.getOriginalFilename();
-			        }
-			    });
-			    // Add entityId and entityName to the request body
-			    body.add("entityId", String.valueOf(entityId));
-			    body.add("entityName", entityName);
-			    body.add("projectId", String.valueOf(projectId));
-			    body.add("questionMasterId", String.valueOf(questionMasterId));
+	// if(!meiliDataList.isEmpty()){
+	// searchUtils.addData("tags", meiliDataList);
+	// }
 
-			    HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
-			    String syncResponse = restTemplate.postForObject(dmsPortalUploadUrl, requestEntity, String.class);
-			    JSONObject json = new JSONObject(syncResponse);
-			    if (!json.isNull("message")) {
-			        String obj = json.getString("message");
-			        if (obj != null) {
-			        	response.setServiceMessage(obj);
-			        }
-			    }
-			}
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		return response;
-	}
-	
+	// if (!savedTags.isEmpty()) {
+	// response.setServiceMessage("Success");
+	// response.setServiceStatus(ServiceResponse.STATUS_SUCCESS);
+	// }else {
+	// response.setServiceMessage("Failed");
+	// response.setServiceStatus(ServiceResponse.STATUS_FAIL);
+	// }
+	// } else {
+	// response.setServiceMessage("No New Tags to Save");
+	// response.setServiceStatus(ServiceResponse.STATUS_SUCCESS);
+	// }
+	// }catch(Exception e) {
+	// e.printStackTrace();
+	// }
+	// return response;
+	// }
+
+	// //Share file (pdf , image, excel, word etc) to python for OCR
+	// public ServiceResponse sendFileForOCR(MultipartFile document, Long entityId,
+	// String entityName, Long questionMasterId, Long projectId) {
+	// ServiceResponse response = new ServiceResponse();
+	// try {
+	// if (document != null) {
+	// String dmsPortalUploadUrl = pythonUrl + "/upload";
+	// RestTemplate restTemplate = new RestTemplate();
+
+	// HttpHeaders headers = new HttpHeaders();
+	// headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+	// MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+	// body.add("file", new ByteArrayResource(document.getBytes()) {
+	// @Override
+	// public String getFilename() {
+	// return document.getOriginalFilename();
+	// }
+	// });
+	// // Add entityId and entityName to the request body
+	// body.add("entityId", String.valueOf(entityId));
+	// body.add("entityName", entityName);
+	// body.add("projectId", String.valueOf(projectId));
+	// body.add("questionMasterId", String.valueOf(questionMasterId));
+
+	// HttpEntity<MultiValueMap<String, Object>> requestEntity = new
+	// HttpEntity<>(body, headers);
+	// String syncResponse = restTemplate.postForObject(dmsPortalUploadUrl,
+	// requestEntity, String.class);
+	// JSONObject json = new JSONObject(syncResponse);
+	// if (!json.isNull("message")) {
+	// String obj = json.getString("message");
+	// if (obj != null) {
+	// response.setServiceMessage(obj);
+	// }
+	// }
+	// }
+	// }catch(Exception e) {
+	// e.printStackTrace();
+	// }
+	// return response;
+	// }
 
 }
