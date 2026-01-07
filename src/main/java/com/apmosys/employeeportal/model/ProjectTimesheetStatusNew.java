2650abdf7e04437dd29ff4966082b0daacb8de94
anyush panda <anyush.panda@apmosys.com>
2026-01-07 12:23:53 +0530

    New updated form for integrating dynamic data

--- src/main/java/com/apmosys/employeeportal/model/ProjectTimesheetStatusNew.java
+++ src/main/java/com/apmosys/employeeportal/model/ProjectTimesheetStatusNew.java
@@ -1,7 +1,5 @@
 package com.apmosys.employeeportal.model;
 
-import java.time.LocalDateTime;
-
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
