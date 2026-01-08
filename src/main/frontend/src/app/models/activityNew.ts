1dabda1de5ffbbdabd43a76116e57903d239c938
anyush panda <anyush.panda@apmosys.com>
2026-01-08 11:04:42 +0530

    project activity mmodel changes

--- src/main/frontend/src/app/models/activityNew.ts
+++ src/main/frontend/src/app/models/activityNew.ts
@@ -1,13 +1,6 @@
 export class ActivityNew {
-  clientId: number | null;
-  clientLocationId: number | null;
-  teamId: number | null;
   activityId: number | null;
   description: string;
   completionTime: number | null;
-  
-  // Additional fields for dropdowns
-  clientLocationList?: any[];
-  projectList?: any[];
   projectActivities?: any[];
 }
