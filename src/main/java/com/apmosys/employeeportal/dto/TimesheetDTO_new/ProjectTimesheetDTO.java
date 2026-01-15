fd35e6804d933080dad86900836d2b75da024669
raj.kumar <raj.kumar@apmosys.com>
2026-01-15 17:55:29 +0530

    added validation for half day and assigment validation(team id moved to activity level)

--- src/main/java/com/apmosys/employeeportal/dto/TimesheetDTO_new/ProjectTimesheetDTO.java
+++ src/main/java/com/apmosys/employeeportal/dto/TimesheetDTO_new/ProjectTimesheetDTO.java
@@ -1,10 +1,6 @@
 package com.apmosys.employeeportal.dto.TimesheetDTO_new;
 
-import java.time.LocalDateTime;
 import java.util.List;
-
-
-import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -145,6 +141,7 @@ public class ProjectTimesheetDTO {
     private String description;
 
     private String clientSideId;
+    
   
 }
 
