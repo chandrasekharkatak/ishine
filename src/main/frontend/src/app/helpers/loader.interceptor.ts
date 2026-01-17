b17dd6546c57c0719085067a5f928706a62250a4
sakti.das <sakti.das@apmosys.com>
2026-01-17 13:19:33 +0530

    1.Loader api added for doc preview and Doc download

--- src/main/frontend/src/app/helpers/loader.interceptor.ts
+++ src/main/frontend/src/app/helpers/loader.interceptor.ts
@@ -453,7 +453,9 @@ export class LoaderInterceptor implements HttpInterceptor {
     `${this.baseUrl}` + `api/getProjectByMonthRangeAndEmpId`,
     `${this.baseUrl}` + `api/getProjectListForDateAndEmpId`,
     `${this.baseUrl}` + `api/getClientDetailsByProjectIdAndEmpId`,
-    `${this.baseUrl}` + `api/getOtherTeamMembersByDateAndProjectId`
+    `${this.baseUrl}` + `api/getOtherTeamMembersByDateAndProjectId`,
+    `${this.baseUrl}` + `api/getDocumentsBySelectedEmpId`,
+    `${this.baseUrl}` + `api/downloadFinalDocuments`
   ]
 
   constructor(private loaderService: LoaderService) { }
