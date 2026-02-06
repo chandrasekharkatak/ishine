b53136120b08147b3ef87e6aa65accaf4599a73e
raj.kumar <raj.kumar@apmosys.com>
2026-02-06 17:24:24 +0530

    minor query bug

--- src/main/java/com/apmosys/employeeportal/repository/TrainingMasterRepository.java
+++ src/main/java/com/apmosys/employeeportal/repository/TrainingMasterRepository.java
@@ -35,6 +35,6 @@ public interface TrainingMasterRepository extends JpaRepository<TrainingMaster, 
 	@Query("SELECT t FROM TrainingMaster t WHERE LOWER(t.trainingName) = LOWER(:trainingName)")
 	Optional<TrainingMaster> findByTrainingNameIgnoreCase(@Param("trainingName") String trainingName);
 	
-	@Query("SELECT t FROM TrainingMaster t WHERE LOWER(t.trainingName) = LOWER(:trainingName) AND tm.trainingId != :trainingId")
+	@Query("SELECT t FROM TrainingMaster t WHERE LOWER(t.trainingName) = LOWER(:trainingName) AND t.trainingId != :trainingId")
 	Optional<TrainingMaster> findByTrainingNameAndNotTrainingId(@Param("trainingName") String trainingName, @Param("trainingId") Integer trainingId);
 }
