f9bfb88a14cf5600b0178c3713891b60da264987
Shikha Suman <shikha.suman@apmosys.com>
2026-02-16 10:40:12 +0530

    1.roleName added in /sendTimesheetDetailsToShankh api and related query changes.2.In Timesheet api et.dayType != 'Leave' updated for counting timesheet count.

--- src/main/java/com/apmosys/employeeportal/repository/EmployeeTeamMapRepository.java
+++ src/main/java/com/apmosys/employeeportal/repository/EmployeeTeamMapRepository.java
@@ -518,10 +518,9 @@ List<Long> findShadowMembersByEmpIdsAndProjectId(@Param("empIds") List<Long> emp
 	 			+ "	INNER JOIN employee_team_mapping etm ON etm.team_id = t.team_id "
 	 			+ "	INNER JOIN employee e ON e.emp_id = etm.emp_id \n"
 	 			+ " INNER JOIN project_po_details ppo on ppo.project_id = p.project_id "
-	 			+ " INNER JOIN po_details pd on pd.po_id = ppo.po_id "
 	 			+ "	WHERE p.active = 'true' AND t.is_active != 'N' "
 	 			+ "	   AND etm.active != 0 AND ppo.active=1 "
-	 			+ "    AND CURDATE() BETWEEN pd.po_start_date AND pd.po_end_date "
+	 			+ "    AND CURDATE() BETWEEN ppo.po_start_date AND ppo.po_end_date "
 	 			+ "	GROUP BY ppo.po_id ",nativeQuery = true)
 				List<Object> getAllApprovedPoWithTimesheet(); 
 	 
