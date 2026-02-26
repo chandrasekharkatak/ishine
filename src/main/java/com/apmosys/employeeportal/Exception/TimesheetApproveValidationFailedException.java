dc24b7eee079627bea909441096a18303d757e22
nabarun.samanta@apmosys.com <nabarun.samanta@apmosys.com>
2026-02-26 11:16:57 +0530

    new exception added

---
+++ src/main/java/com/apmosys/employeeportal/Exception/TimesheetApproveValidationFailedException.java
@@ -0,0 +1,7 @@
+package com.apmosys.employeeportal.Exception;
+
+public class TimesheetApproveValidationFailedException extends RuntimeException {
+    public TimesheetApproveValidationFailedException(String message) {
+		super(message);
+	}
+}
