aa4eaafae322dfd41e61b2900ceebf65163222c0
anyush panda <anyush.panda@apmosys.com>
2026-03-09 19:16:58 +0530

    1. Apply Filter function. 2. Extra tab removed from Team TImesheets

--- src/main/frontend/src/app/user-timesheet/team-timesheet/team-timesheet.component.ts
+++ src/main/frontend/src/app/user-timesheet/team-timesheet/team-timesheet.component.ts
@@ -233,8 +233,8 @@ rejectionReasons:any;
 
   ) {
     this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
-    this.safePdfUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
-  'assets/Ishine_Timesheet_TNM.pdf');
+  //   this.safePdfUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+  // 'assets/Ishine_Timesheet_TNM.pdf');
   }
 
   async ngOnInit(): Promise<void> {
@@ -301,9 +301,7 @@ rejectionReasons:any;
   }
 
   sectionViewInit() {
-    if (this.userMapping.view_my_teams_timesheets) {
-      this.showAllTimesheetsTable();
-    } else if (this.userMapping.view_my_teams_timesheets_requests || this.userMapping.update_timesheet_request || this.userMapping.revoke_reportee_timesheet) {
+   if (this.userMapping.view_my_teams_timesheets_requests || this.userMapping.update_timesheet_request || this.userMapping.revoke_reportee_timesheet) {
       this.showAllTimesheetRequestsTable();
     }
   }
