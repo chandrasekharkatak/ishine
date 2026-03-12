81620ecb58dc80e73e16c31ce65e3fc57f57c40b
ayush.mishra <ayush.mishra@apmosys.com>
2026-03-12 17:50:16 +0530

    1.approval query fix

--- src/main/java/com/apmosys/employeeportal/repository/EmployeeTimesheetsNewRepository.java
+++ src/main/java/com/apmosys/employeeportal/repository/EmployeeTimesheetsNewRepository.java
@@ -17235,7 +17235,7 @@ countQuery = "SELECT COUNT(DISTINCT etn.timesheetId) " +
 				"p.projectName, c.clientName, cl.clientLocation, ptsn.poNo, \n" +
 				"es.name, ptsn.status, ptsn.totalClientWorkingMinutes, ptsn.description, \n" +
 				"etamn.timesheetId, etamn.locationMappingId, etamn.projectId, \n" +
-				"a.activity, etamn.description, etamn.durationMinutes, \n" +
+				"etamn.id , a.activity, etamn.description, etamn.durationMinutes, \n" +
 				"t.teamName, \n" +
 				"tddn.docId, tddn.docName, tddn.finalFlag, tddn.bulkApprovedDocId, dmtmn.mimeType, tddn.projectId, \n" +
 				"trdn.timesheetId, trdn.locationMappingId, trdn.projectId, trrm.rejectionReason, trdn.remarks, trdn.rejectedOn"
