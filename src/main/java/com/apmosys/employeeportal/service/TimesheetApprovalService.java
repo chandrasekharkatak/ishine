9a1f36ad5caaad43a4d63a5d116bfc5caa7f10ff
raj.kumar <raj.kumar@apmosys.com>
2026-04-09 10:59:46 +0530

    missing method

--- src/main/java/com/apmosys/employeeportal/service/TimesheetApprovalService.java
+++ src/main/java/com/apmosys/employeeportal/service/TimesheetApprovalService.java
@@ -2096,6 +2096,7 @@ public ServiceResponse bulkOrSingleApproveOrReject(BulkTimesheetRequestDTO reque
             if ("APPROVED".equals(status) && Boolean.TRUE.equals(ts.getIsWorkingDay())) {
 
             // Check: client-side project but no docs
+            if (isMissingClientSideDocs(ts.getTimesheetId(),projectIds, docs, projectClientSideMap)) {
                 skippedTimesheets.add(new SkippedTimesheetDTO(
                         ts.getTimesheetId(),
                         formattedEmpId,
