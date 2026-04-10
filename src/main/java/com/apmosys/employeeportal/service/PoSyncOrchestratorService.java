25143c91ba739ec9def45e641c9ba9a5043c1fd4
Upendra <upendra.singh@apmosys.com>
2026-04-10 16:03:15 +0530

    renew mail

--- src/main/java/com/apmosys/employeeportal/service/PoSyncOrchestratorService.java
+++ src/main/java/com/apmosys/employeeportal/service/PoSyncOrchestratorService.java
@@ -125,6 +125,9 @@ public class PoSyncOrchestratorService {
 	ClientsRepository clientRepository;
 	
 	@Autowired
+	CronJobService cronJobService;
+	
+	@Autowired
 	MailService mailService;
 
 	@Autowired
@@ -444,7 +447,17 @@ public class PoSyncOrchestratorService {
 
 		poDetailsService.validateAssociatedPosIntegrity(project.getProjectId(), dto.getAssociatePosAfterRenewal());
 		poDetailsService.updatePoLinksAfterRenewal(project.getProjectId(), dto);
-		teamsService.migrateResourcesAfterRenewal(project.getProjectId(), newPo.getPoId(), dto.getRenewedByEmpId());
+		List<AutoMigrationDTO> autoMigrated =
+		        teamsService.migrateResourcesAfterRenewalDTO(
+		                project.getProjectId(),
+		                newPo.getPoId(),
+		                dto.getRenewedByEmpId()
+		        );
+
+		if (!autoMigrated.isEmpty()) {
+			cronJobService.sendAutoMigrationMail(autoMigrated);
+		}
+
 		projectService.recalculateProjectDates(project.getProjectId(), true);
 
 		applyRenewPoSuccess(response);
