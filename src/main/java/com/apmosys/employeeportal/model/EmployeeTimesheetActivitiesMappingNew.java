4627b1c124cc3dac03a71ec88b100c5a25258b62
ayush.mishra <ayush.mishra@apmosys.com>
2026-05-08 16:29:00 +0530

    1.Adding @Audited

--- src/main/java/com/apmosys/employeeportal/model/EmployeeTimesheetActivitiesMappingNew.java
+++ src/main/java/com/apmosys/employeeportal/model/EmployeeTimesheetActivitiesMappingNew.java
@@ -7,6 +7,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.hibernate.envers.Audited;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +17,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Audited
 @Table(name = "employee_timesheet_activities_mapping_new")
 public class EmployeeTimesheetActivitiesMappingNew {
 
