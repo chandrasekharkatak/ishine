6f9c0ac7017e3ffd9b313fce1fa197a21557b4a3
anyush.panda <anyush.panda@apmosys.com>
2026-05-19 13:46:16 +0530

    Revert "config removed"
    This reverts commit 2f9b00d1a2358ae32590296d040fd1151585368e.

--- src/main/java/com/apmosys/employeeportal/MyConfig.java
+++ src/main/java/com/apmosys/employeeportal/MyConfig.java
@@ -29,7 +29,13 @@ public class MyConfig implements WebMvcConfigurer {
     @Value("${security.csp.policy}")
     private String contentSecurityPolicy;
 
-
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/index.html")
+                .addResourceLocations("classpath:/static/index.html")
+                .setCacheControl(CacheControl.noCache());
+    }
     
     
     @Override
