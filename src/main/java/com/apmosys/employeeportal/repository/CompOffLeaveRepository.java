36d5379bdaf078dc002772984855f5bc2c4c05d5
nabarun.samanta@apmosys.com <nabarun.samanta@apmosys.com>
2026-05-27 18:23:52 +0530

    compOff expiry fix

--- src/main/java/com/apmosys/employeeportal/repository/CompOffLeaveRepository.java
+++ src/main/java/com/apmosys/employeeportal/repository/CompOffLeaveRepository.java
@@ -43,7 +43,7 @@ public interface CompOffLeaveRepository extends JpaRepository<CompOffLeave, Long
 	public CompOffLeave findOldestCompOffApplicationByEmpId(Long empId, String compOffStatus);
 
 	@Query(nativeQuery = true)
-	public List<CompOffLeave> findAllPendingApplicationByEmpId(Long empId, Timestamp perv45Day, String compOffStatus);
+	public List<CompOffLeave> findAllPendingApplicationByEmpId(Long empId,  String compOffStatus);
 
 	public List<CompOffLeave> findByEmpId(Long empId);
 
